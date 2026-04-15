dc161aa628bf68b533ebe10f20b75dae8aa1258e
Julie-sx <julie.sieux@efrei.net>
2026-04-15 11:00:12 +0200

    Finalisation menu

---
+++ js/Java.js
@@ -0,0 +1,18 @@
+// Fonction pour le header responsive
+
+function afficherMenu () {
+    let menu = document.getElementById("liens");
+    if (menu.style.display === "none") {
+        menu.style.display = "flex";
+    }
+    else {
+        menu.style.display = "none";
+    }
+    return;
+}
+
+window.addEventListener("resize", function () {
+    if (window.innerWidth > 768) {
+        document.getElementById("liens").style.display = "flex";
+    }
+});
